a5023f0257381afaa140450bd77945d9d49d8db2
Axcel Joh H. Bernal <148522639+AJ-programmer@users.noreply.github.com>
2025-05-10 08:22:27 +0800

    Create LoginActivity.java

---
+++ LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.loginregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
+    DbHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username1);
+        password = (EditText) findViewById(R.id.Password1);
+        btnLogin = (Button) findViewById(R.id.btnsignin1);
+        DB = new DbHelper(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(LoginActivity.this, "Please enter all the field", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkuserpass =  DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(LoginActivity.this,"Sign in successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Invalid Credential", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+}
